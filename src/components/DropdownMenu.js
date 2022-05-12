8e6e4d229fe09097cee3e336555f1e800a997ce0
Robin Lejeune <robinlejeune@gmail.com>
2022-05-12 09:33:24 +0200

    Login/Signup - Dropdown for the promotion

---
+++ src/components/DropdownMenu.js
@@ -0,0 +1,18 @@
+import './stylesheets/dropdownmenu.css'
+
+const DropdownMenu = ({ children, name }) => {
+
+  // to be replaced by a fetch to the API
+  const options = ['Johnson 7', 'Hopper 3', 'Bouman 4', 'Lovelace 8', 'Swartz 6']
+
+  return (
+    <select className='dropdown-select' name={name} id={name}>
+      <option value=''>{children}</option>
+      {options.map(option => (
+        <option value={option}>{option}</option>
+      ))}
+    </select>
+  )
+}
+
+export default DropdownMenu
--- src/components/LogForm.js
+++ src/components/LogForm.js
@@ -1,4 +1,5 @@
 import Button from './Button'
+import DropdownMenu from './DropdownMenu'
 import InnerLabelInput from './InnerLabelInput'
 import './stylesheets/logform.css'
 
@@ -14,10 +15,10 @@ const LogForm = ({ isLogin }) => {
 
   const innerForm = isLogin ? (
     <form method='POST' action='' className='log-form log-form--login'>
-      <InnerLabelInput name='email' type='email'>
+      <InnerLabelInput name='email' type='email' key='email'>
         E-mail
       </InnerLabelInput>
-      <InnerLabelInput name='password' type='password'>
+      <InnerLabelInput name='password' type='password' key='password'>
         Password
       </InnerLabelInput>
       <Button className='btn-secondary log-form--button' handleClick={login}>
@@ -32,15 +33,16 @@ const LogForm = ({ isLogin }) => {
       <InnerLabelInput name='lastname' type='text'>
         Last name
       </InnerLabelInput>
-      <InnerLabelInput name='email' type='email'>
+      <InnerLabelInput name='email' type='email' key='email'>
         E-mail
       </InnerLabelInput>
-      <InnerLabelInput name='password' type='password'>
+      <InnerLabelInput name='password' type='password' key='password'>
         Password
       </InnerLabelInput>
       <InnerLabelInput name='confirmpassword' type='password'>
         Confirm your password
       </InnerLabelInput>
+      <DropdownMenu name='promotion'>-- Select your promotion --</DropdownMenu>
       <Button className='btn-secondary log-form--button' handleClick={signup}>
         Register
       </Button>
