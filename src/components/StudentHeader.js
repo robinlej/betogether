017bf73a77fc3cb4cbcce02485bee0adfffbf6af
Robin Lejeune <robinlejeune@gmail.com>
2022-05-13 10:08:29 +0200

    StudentHeader up to date with the dashboard

--- src/components/StudentHeader.js
+++ src/components/StudentHeader.js
@@ -1,7 +1,9 @@
 import React from "react";
 import {Nav, NavLink, NavBtn, NavBtnLink, Logout} from './StudentHeaderElements'
-import logo from "../assets/logo-becode.png";
+import DashboardProgress from './DashboardProgress'
+import UserProfilePreview from './UserProfilePreview'
 import StudentProfileAvatar from "./StudentProfileAvatar";
+import logo from "../assets/logo-becode.png";
 
 function StudentHeader({page}) {
   return (
@@ -10,8 +12,9 @@ function StudentHeader({page}) {
         <NavLink to="/">
           <img src={logo} style={{"height": "5rem"}} alt="Logo" />
         </NavLink>
+        { page === 'dashboard' && <DashboardProgress />}
         <div style={{"display":"flex", "gap":"4rem"}}>
-        { page === 'profile' && < StudentProfileAvatar /> }
+        { page !== 'profile' && <UserProfilePreview name="Jane Doe" promotion='Johnson 7' /> }
         <NavBtn>
           <NavBtnLink to="/login"><Logout></Logout>Logout</NavBtnLink>
         </NavBtn>
