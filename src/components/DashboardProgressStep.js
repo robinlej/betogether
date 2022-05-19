24dc25def6cdf82d446c55fad26158749e129565
Robin Lejeune <robinlejeune@gmail.com>
2022-05-19 14:30:49 +0200

    CSS modification on DashboardProgressStep

--- src/components/DashboardProgressStep.js
+++ src/components/DashboardProgressStep.js
@@ -1,6 +1,6 @@
 const DashboardProgressStep = ({ step, number, children }) => {
   return (
-    <div className={`dashboard-progress--step ${ step >= number ? 'active' : '' }`}>
+    <div className={`dashboard-progress--step ${ step === number ? 'active' : step > number ? 'past-active' : null }`}>
       <div className='dashboard-progress--step--number'>{number}</div>
       <div className='dashboard-progress--step--label'>{children}</div>
     </div>
