7cd6f87204696c4002441aaea6775cb02b046520
Robin Lejeune <robinlejeune@gmail.com>
2022-05-24 16:24:35 +0200

    Dashboard Step 1 form - Fetch API + header logout

--- src/components/OuterLabelInput.js
+++ src/components/OuterLabelInput.js
@@ -1,12 +1,12 @@
 import './stylesheets/OuterLabelInput.css'
 
-const OuterLabelInput = ({ children, name, type }) => {
+const OuterLabelInput = ({ children, name, type, handleInputChange }) => {
   return (
     <div className='outer-label-wrap'>
       <label className='outer-label' htmlFor={name}>
         {children}
       </label>
-      <input type={type} name={name} id={name} />
+      <input type={type} name={name} id={name} onChange={handleInputChange} />
     </div>
   )
 }
--- src/components/StudentHeader.js
+++ src/components/StudentHeader.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import {Nav, NavLink, NavBtn, NavBtnLink, Logout} from './StudentHeaderElements'
 import DashboardProgress from './DashboardProgress'
 import UserProfilePreview from './UserProfilePreview'
 // import StudentProfileAvatar from "./StudentProfileAvatar";
 import logo from "../assets/logo-becode.png";
 import { UploadBtn } from "./StudentProfilePageComponents";
+import { setCookie, UserContext } from "../App";
 
 function StudentHeader({ page, step }) {
+  const {setToken} = useContext(UserContext)
+
+  const signOut = () => {
+    setToken(null)
+    setCookie('token', '', 0)
+  }
+
   return (
     <>
       <Nav>
@@ -17,8 +25,10 @@ function StudentHeader({ page, step }) {
         <div style={{"display":"flex", "gap":"4rem"}}>
         { (page !== 'profile' && page !== 'welcome') && <UserProfilePreview name="Jane Doe" promotion='Johnson 7' /> }
         <NavBtn>
-        { page === 'welcome' && <NavLink to='/profile'><UploadBtn style={{ "marginRight":"2rem"}}>Edit my profile</UploadBtn></NavLink>}
-        <NavLink to='/'><NavBtnLink><Logout></Logout>Logout</NavBtnLink></NavLink>
+          { page === 'welcome' && <NavLink to='/profile'><UploadBtn style={{ "marginRight":"2rem"}}>Edit my profile</UploadBtn></NavLink>}
+          <NavLink to='/' onClick={signOut}>
+            <NavBtnLink><Logout></Logout>Logout</NavBtnLink>
+          </NavLink>
         </NavBtn>
         </div>
       </Nav>
