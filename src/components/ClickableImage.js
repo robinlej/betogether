0dc0b4531ecf2ccee4e9a6825f18656df9c8fed0
Robin Lejeune <robinlejeune@gmail.com>
2022-05-31 10:22:21 +0200

    Clickable images on project preview

--- src/components/ClickableImage.js
+++ src/components/ClickableImage.js
@@ -1,21 +1,41 @@
+import { useState } from 'react'
 import { BiExpand } from 'react-icons/bi'
 import './stylesheets/ClickableImage.css'
 
 const ClickableImage = ({ src }) => {
+  const [isExpanded, setIsExpanded] = useState(false)
 
-  function expandImg {
-    // show big picture
+  function expandImg(e) {
+    e.stopPropagation()
+    setIsExpanded(true)
+  }
+
+  function shrinkImg(e) {
+    e.stopPropagation()
+    setIsExpanded(false)
   }
 
   return (
-    <div className="clickable-img__wrapper">
-      <div className="clickable-img__container">
-        <img className="clickable-img" tabindex="0" src={src} onClick={expandImg} />
-        <div className="clickable-img__overlay">
-          <BiExpand />
+    <>
+      <div className='clickable-img__wrapper' onClick={expandImg}>
+        <div className='clickable-img__container'>
+          <img className='clickable-img' tabindex='0' src={src} />
+          <div className='clickable-img__overlay'></div>
         </div>
+        <BiExpand className='clickable-img__overlay--icon' />
       </div>
-    </div>
+
+      {isExpanded && (
+        <div className='clickable-img--expanded__container' onClick={shrinkImg}>
+          <img
+            className='clickable-img--expanded'
+            tabindex='0'
+            src={src}
+            loading='lazy'
+          />
+        </div>
+      )}
+    </>
   )
 }
 
